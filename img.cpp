1f14e1cd70eb3831ca9db6e28cc26fd10774532c
Alexey Volchenkov <altay197@yandex.ru>
2021-07-08 12:29:38 +0300

    add trajectory for ball

--- img.cpp
+++ img.cpp
@@ -1,58 +1,120 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <cmath>
 
-struct Image {
-	int mx;
-	int my;
-	int* points;
+using namespace std;
+const int g = 10;
+
+class Image {
+    int mx;
+    int my;
+    int* points;
+public:
+    ~Image()
+    {
+        //delete[] points;
+    }
+    void createImage(int tmx, int tmy)
+    {
+        mx = tmx;
+        my = tmy;
+        points = new int[tmx * tmy]{ 0 };
+    }
+
+    void showImage()
+    {
+        for (int y = 0; y < my; y++)
+        {
+            for (int x = 0; x < mx; x++)
+            {
+                cout << points[y * mx + x] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void setPointImage(int x, int y, int color)
+    {
+        points[y * mx + x] = color;
+    }
+
+    int getPointImage(int x, int y)
+    {
+        return points[y * mx + x];
+    }
+
+    void saveToPnmFile(string fileName)
+    {
+        fstream file;
+        file.open(fileName);
+        file << "P1" << endl;
+        file << mx << " " << my << endl;
+        for (int y = 0; y < my; y++)
+        {
+            for (int x = 0; x < mx; x++)
+            {
+                file << points[y * mx + x] << " ";
+            }
+            file << endl;
+        }
+        file.close();
+    }
+};
+
+class Ball {
+    double m;
+    double v = 0;
+    double h;
+public:
+    Ball(double m = 0, double h = 0)
+    {
+        this->m = m;
+        this->h = h;
+    }
+
+    void show()
+    {
+        cout << "h=" << round(h) << endl;
+        cout << "v=" << round(v) << endl;
+        cout << "=====" << endl;
+    }
+
+    void foo(double percent, int n, Image& img, int w)
+    {
+        double E = m * g * h;
+        v = sqrt((2 * E) / m);
+
+        for (int i = 0; i < n; i++)
+        {
+            show();
+            for (int j = 0; j <= w; j++)
+            {
+                if (j >= abs(round(h) - w))
+                {
+                    img.setPointImage(i, j, 1);
+                }
+            }
+
+            if (i != n - 1)
+            {
+                E -= E * (percent / 100);
+                h = E / (m * g);
+                v = sqrt((2 * E) / m);
+            }
+        }
+    }
 };
 
 
+int main()
+{
+    Image img;
+    int w = 10;
+    img.createImage(5, w);
+    Ball ball(10, 10);
+    ball.foo(50, 3, img, w);
 
-void createImage(Image* img, int tmx, int tmy){
-	img->mx = tmx;
-	img->my = tmy;
-	img->points = new int[tmx * tmy]{ 0 };
-}
-
-
-void showImage(Image* img){
-	for (int y = 0; y < img->my; y++) {
-		for (int x = 0; x < img->mx; x++) {
-			cout << img->points[y * img->mx + x] << " ";
-		}
-		cout << endl;
-	}
-}
-
-
-void setPointImage(Image* img, int x, int y, int color){
-	img->points[y * img->mx + x] = color;
-}
-
-
-int getPointImage(Image* img, int x, int y, int color){
-	return img->points[y * img->mx + x] = color;
-}
-
-
-void saveToPnmFile(Image* img, string fileName) {
-	fstream file(fileName);
-	file << "P1" << endl;
-	file << img->mx << " " << img->my << endl;
-	for (int y = 0; y < img->my; y++) {
-		for (int x = 0; x < img->mx; x++) {
-			file << img->points[y * img->mx + x] << " ";
-		}
-		file << endl;
-	}
-	file.close();
-}
-int main() {
-	Image* img = new Image();
-	createImage(img, 6, 10);
-	setPointImage(img, 5, 9, 1);
-	showImage(img);
-	saveToPnmFile(img, "pic1.pnm");
+    img.showImage();
+    img.saveToPnmFile("pic1.pnm");
+    return 0;
 }
